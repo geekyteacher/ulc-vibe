e4252389eef800b35b97deee02293e4ec40aae27
autochilli <si@geeky-teacher.co.uk>
2019-06-08 17:38:14 +0800

    update store

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from store.models import Category, Product
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name","description")
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- store/models.py
+++ store/models.py
@@ -5,10 +5,48 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField()
+    parent = models.ForeignKey('self',blank=True, null=True ,related_name='children', on_delete=models.PROTECT)
 
     class Meta:
         verbose_name = "Category"
         verbose_name_plural = "Categories"
+        unique_together = ('name', 'parent',)    #enforcing that there can not be two
+
+    """ def __str__(self):
+        return self.name """
+    def __str__(self):                           # __str__ method elaborated later in
+        full_path = [self.name]                  # post.  use __unicode__ in place of
+                                                 # __str__ if you are using python 2
+        k = self.parent
+
+        while k is not None:
+            full_path.append(k.name)
+            k = k.parent
+
+        return ' -> '.join(full_path[::-1])
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    price = models.DecimalField(max_digits = 5, decimal_places = 2)
+    image =models.FilePathField(path="/img")
+    created_on = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
 
     def __str__(self):
-        return self.name
+        return self.title
+
+    def get_cat_list(self):           #for now ignore this instance method,
+        k = self.category
+        breadcrumb = ["dummy"]
+        while k is not None:
+            breadcrumb.append(k.slug)
+            k = k.parent
+
+        for i in range(len(breadcrumb)-1):
+            breadcrumb[i] = '/'.join(breadcrumb[-1:i-1:-1])
+        return breadcrumb[-1:0:-1]
