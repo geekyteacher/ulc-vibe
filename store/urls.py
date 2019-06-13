f3b1e72b0036f5be109d35b5567a4c2302a46ba1
CSSFA <si@autochilli.com>
2019-06-13 14:40:36 +0800

    update store URLs

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.store_index, name="store_index"),
-    path('cat/',views.store_categories, name="store_categories")
+    #path('', views.store_index, name="store_index"),
+    path('',views.store_categories, name="store_categories"),
 ]
