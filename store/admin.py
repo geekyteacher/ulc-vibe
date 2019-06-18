df00286bffe932ad9af2a9414c9938dfe947ca59
CSSFA <si@autochilli.com>
2019-06-18 10:51:43 +0800

    added sizes table

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from store.models import Category, Product, ProductPicture
+from store.models import Category, Product, ProductPicture, Size
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
@@ -13,5 +13,10 @@ class ProductAdmin(admin.ModelAdmin):
         ProductPictureInline,
     ]
 
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ("name","description")
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Size, SizeAdmin)
