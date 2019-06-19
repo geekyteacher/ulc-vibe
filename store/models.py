4eacbfff1b3b616fad507027d4d7bda60af84ad8
CSSFA <si@autochilli.com>
2019-06-19 15:34:03 +0800

    added product code field

--- store/models.py
+++ store/models.py
@@ -59,6 +59,7 @@ class Product(models.Model):
         (FEMALE, "Female"),
     ]
 
+    productCode=models.CharField(max_length=30, null=False, unique=True)
     name = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(max_digits = 5, decimal_places = 2)
