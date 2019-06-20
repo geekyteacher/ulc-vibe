d34734821cb0f0c1beac21f0889dfcc7f28d1fd7
CSSFA <si@autochilli.com>
2019-06-20 14:23:40 +0800

    messing with store

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     #path('', views.store_index, name="store_index"),
     path('',views.store_index, name="store_index"),
     path('<pk>/',views.store_category, name="store_category"),
+    path('<pk>/',views.store_subcategory, name="store_subcategory"),
 ]
--- store/views.py
+++ store/views.py
@@ -13,14 +13,36 @@ def store_index(request):
 def store_category(request, pk):
     
     products = Product.objects.all()
-    cat_id = Category.objects.get(pk=pk)
+    #catName = Category.objects.get(name=name)
+    catID = Category.objects.get(pk=pk)
     categories = Category.objects.filter(
-        parent_id__name__contains=cat_id
+        parent_id__name__contains=catID
     )
     context = {
-        "category":cat_id,
+        #"catName":catName,
+        "category":catID,
         "categories":categories,
         "activate":"store",
         "products":products,
     }
     return render(request, "store_category.html", context)
+
+def store_subcategory(request, parent_pk, pk):
+    
+    products = Product.objects.all()
+    
+    catID = Category.objects.get(pk=pk)
+    categories = Category.objects.filter(
+        parent_id__name__contains=catID
+    )
+    context = {
+        "category":catID,
+        "categories":categories,
+        "activate":"store",
+        "products":products,
+    }
+    return render(request, "store_subcategory.html", context)
+
+def store_product(request, item):
+    item = Product.objects.all()
+    return render(request, "store_product.html", context)
